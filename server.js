afdbafe413c6eb138926a376bca238a362ea5202
sbahlool <saud.bahlool@hotmail.com>
2026-08-14 19:24:42 +0300

    return cors

--- server.js
+++ server.js
@@ -16,28 +16,28 @@ const app = express()
 // temporary permissive CORS (for testing only)
 app.use(require('cors')())
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       const allowedOrigins = [
-//         'http://methleague.surge.sh',
-//         'https://methleague.surge.sh',
-//         'http://localhost:5173'
-//       ]
-//       if (
-//         !origin ||
-//         allowedOrigins.indexOf(origin) !== -1 ||
-//         process.env.NODE_ENV === 'development'
-//       ) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error('Not allowed by CORS'))
-//       }
-//     },
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-//   })
-// )
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        'http://methleague.surge.sh',
+        'https://methleague.surge.sh',
+        'http://localhost:5173'
+      ]
+      if (
+        !origin ||
+        allowedOrigins.indexOf(origin) !== -1 ||
+        process.env.NODE_ENV === 'development'
+      ) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
